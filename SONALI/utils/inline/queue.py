a7e8f36427fab561008fe7498e317e53c7f8feaa
sachinopboy2 <sachin25u7@gmail.com>
2025-09-14 12:38:41 +0530

    Update queue.py

--- SONALI/utils/inline/queue.py
+++ SONALI/utils/inline/queue.py
@@ -73,10 +73,10 @@ def aq_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="𝗔ʙᴏᴜᴛ 𝗔ʟᴘʜᴀ 🥀", url=f"https://t.me/WORLD_ALPHA"
+                text="ɴᴏʙɪᴛᴀ </> ɴᴇᴛᴡᴏʀᴋ", url=f"https://t.me/NOBITA_BOTS"
             ),
             InlineKeyboardButton(
-                text="𝗦ᴜᴘᴘᴏʀᴛ 𝖦ʀᴏᴜᴘ 🥀", url=f"https://t.me/PURVI_UPDATES"
+                text="𝗦ᴜᴘᴘᴏʀᴛ 𝖦ʀᴏᴜᴘ 🥀", url=f"https://t.me/NOBITA_BOTS"
             ),
         ],
       ]
